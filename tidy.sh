6cf3468d2df623426a7850629620f6256af88c92
Sokolova Daria <dasok2016@gmail.com>
2026-06-05 15:35:20 +0300

    Fixing some errors

--- tidy.sh
+++ tidy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 CHECKS='-*,clang-analyzer-*,performance-*,bugprone-*'
-FILES=$(git ls-files '*.cpp' '*.cxx' '*.cc' | tr '\n' ' ')
+FILES=$(git ls-files '*.cpp' '*.cxx' '*.cc' 2>/dev/null | tr '\n' ' ')
+
+if [[ -z "$FILES" ]]; then
+  FILES=$(find . -maxdepth 1 \( -name '*.cpp' -o -name '*.cxx' -o -name '*.cc' \) -printf '%P ')
+fi
 
 if [[ -z "$FILES" ]]; then
   echo "No source files to analyze."
@@ -12,5 +16,13 @@ set -eo pipefail
 
 for f in $FILES; do
   echo "Running clang-tidy on $f"
-  clang-tidy -p .  -checks="$CHECKS" "$f" --extra-arg="-isystem${PWD}/../external/eigen" --extra-arg="-isystem${PWD}/../external/lazycsv/include" --quiet 2>&1
+  clang-tidy \
+    -checks="$CHECKS" \
+    "$f" \
+    -- \
+    -std=c++17 \
+    -I../external/eigen \
+    -I/usr/include/eigen3 \
+    -I../external/lazycsv/include \
+    -I/usr/include/x86_64-linux-gnu/openblas-pthread/
 done
